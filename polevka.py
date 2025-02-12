4604c2a4b63b3bf33bbd8b9cab2e03d9f8a6385c
kathorak <146837122+kathorak@users.noreply.github.com>
2025-02-12 08:58:25 +0100

    Update polevka.py

--- polevka.py
+++ polevka.py
@@ -1,13 +1,26 @@
 from bs4 import BeautifulSoup
+import json
 import requests
 
 
 def main():
 
+    response = requests.get("https://js-trebesin.github.io/bsoup-exam/")
+    soup = BeautifulSoup(response.content, "html.parser") #vytvoří polévku:) přeloží html pro soup 
 
-    # response = requests.get(url)
+    ingred = soup.find_all("h2")
+    ingred_txt = []
 
-    # BeautifulSoup(response.content, "html.parser") <--- Úkol: popiš krátce, co tohle dělá
+    for i in ingred:
+        ingred_txt.append(i.text)
+    for x in range(4):
+        print(ingred_txt[x])
+
+    
+
+    with open("polevka.json", "w") as data:
+        json.dump(ingred_txt, data, indent=4, ascii=False)
+
 
 
 if __name__ == "__main__":
